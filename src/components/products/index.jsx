053bb93bd036b1d7ee6f6917c61f16360355ddfc
shahin <shahin.shahi1986@gmail.com>
2022-08-01 11:13:53 +0300

    make data file for products

---
+++ src/components/products/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    ProductsContainer,
+    ProductWrapper,
+    ProductsHeading,
+    ProductTitle,
+    ProductCard,
+    ProductImg,
+    ProductInfo,
+    ProductDesc,
+    ProductPrice,
+    ProductButton
+} from './ProductsElements';
+
+const Products = () => {
+    return (
+        <ProductsContainer>
+
+        </ProductsContainer>
+    )
+}
+
+export default Products
