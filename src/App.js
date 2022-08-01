41221d960895c6bc142a2ae2c5fe63df756398ba
shahin <shahin.shahi1986@gmail.com>
2022-08-01 12:01:09 +0300

    make footer desigen

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Hero from "./components/hero";
+import Products from "./components/products";
 
 import {GlobalStyle} from './globalStyle'
-
+import {productData,productDataTwo} from './components/products/data'
+import Feature from "./components/feature";
 
 function App() {
   return (
     <>
     <Router>
-        <GlobalStyle/>
-      
-        <Hero/>
+      <GlobalStyle/>
+      <Hero/>
+      <Products heading='choose your favorite' data={productData}/>
+      <Feature/>
+      <Products heading='Sweet Treats for You' data={productDataTwo} />
     </Router>
     </>
   
--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -12,10 +12,35 @@ import {
     ProductButton
 } from './ProductsElements';
 
-const Products = () => {
+
+const Products = ({ heading, data }) => {
     return (
         <ProductsContainer>
-
+            <ProductsHeading>{heading}</ProductsHeading>
+            <ProductWrapper>
+                {data.map((product, index) => {
+                    return (
+                        <ProductCard key={index}>
+                            <ProductImg src={product.img}
+                                alt={product.alt} />
+                            <ProductInfo>
+                                <ProductTitle>
+                                    {product.name}
+                                </ProductTitle>
+                                <ProductDesc>
+                                    {product.desc}
+                                </ProductDesc>
+                                <ProductPrice>
+                                    {product.price}
+                                </ProductPrice>
+                                <ProductButton>
+                                    {product.button}
+                                </ProductButton>
+                            </ProductInfo>
+                        </ProductCard>
+                    )
+                })}
+            </ProductWrapper>
         </ProductsContainer>
     )
 }
