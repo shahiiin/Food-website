61dc7fcc0ddc5b18cbf34b543054172924458f94
shahin <shahin.shahi1986@gmail.com>
2022-08-01 12:10:16 +0300

    finished all of them

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -6,7 +6,7 @@ const Navbar = ({toggle}) => {
   return (
     <>
       <Nav>
-        <h1 style={{color:"#fff",marginTop:"1rem",fontWight:900}}>Pizza</h1>
+        <h1 style={{color:"#fff",marginTop:"1rem",fontWeight:900}}>Pizza</h1>
         <NavIcon onClick={toggle}>
           <p>menu</p>
           <Bars/>
